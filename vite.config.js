8b0efb378860f7337964c9854dea3bb5736b7895
taikhoanpthan <taikhoanpthan123@gmail.com>
2025-07-01 16:40:59 +0700

    change vercel json

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // Thư mục chứa file build
-  },
-  server: {
-    port: 3000, // Cổng chạy local server
-    open: true, // Tự động mở trình duyệt
-  },
 });
