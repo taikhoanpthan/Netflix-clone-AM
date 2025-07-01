5db646c73d52f62fd7f38d6476963782c4e28cc8
taikhoanpthan <taikhoanpthan123@gmail.com>
2025-07-01 20:08:35 +0700

    lazy load

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"], // Tách riêng thư viện lớn
+        },
+      },
+    },
+  },
 });
